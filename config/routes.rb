324db1414da516553373a137ab494c9d5b632459
Lobzov Alexey <lobzov_lav@mail.ru>
2021-10-01 09:47:06 +0500

    Add comments

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #get "/articles", to: "articles#index"
   #get "/articles/:id", to: "articles#show"
-  resources :articles  # выше два маршрута заменяем на эту запись
-
+  resources :articles  do # выше два маршрута заменяем на эту запись
+    resources :comments
+  end
 end
